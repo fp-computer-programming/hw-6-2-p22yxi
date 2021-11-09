5711415253a4c0a2bab080c2f75a6037b47a66c0
p22yxi <p22yxi@fairfieldprep.org>
2021-11-09 10:19:24 -0500

    hw6-2

---
+++ hw6-2.py
@@ -0,0 +1,30 @@
+# Author Yongdong Xi
+# Question 1
+first_lst = [0, 1, 2, 3, 4, 5, 6, 7]
+
+# Question 2
+print(len(first_lst))
+
+# Question 3
+print(first_lst[2])
+
+# Question 4
+print(5 not in first_lst)
+
+# Question 5
+print(10 not in first_lst)
+
+# Question 6
+name_lst = ['Bonilla', 'Diao', 'Elsheikh', 'Xi']
+
+# Question 7
+print(name_lst[3])
+
+# Question 8
+print(name_lst[3] * 3)
+
+# Question 9
+print(first_lst + name_lst)
+
+# Question 10
+print(len(first_lst + name_lst))
